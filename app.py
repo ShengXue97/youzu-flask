11a92bb72ef332ddf88bab310aea0213c5eab7e9
ShengXue97 <simshengxue1997@gmail.com>
2020-06-08 02:25:21 +0800

    change to dbj

--- app.py
+++ app.py
@@ -32,8 +32,9 @@ def get_message(currentIP, currentTime):
     requestIDRaw = currentIP + "_" + currentTime
     requestIDProcessed = currentTime.replace(":", "-").replace(".", "_")
     print(requestIDProcessed)
-    print(db.getall())
+    #print(db.getall())
     print(currentObj)
+    print("---------------")
     if not db.exists(requestIDProcessed):
         out_dict["ipExists"] = "no"
         out_dict["timeStampExists"] = "no"
