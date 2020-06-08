16a3375f6c4e05b0a96ad88402f47a2463e3c188
ShengXue97 <simshengxue1997@gmail.com>
2020-06-08 15:25:54 +0800

    make db global

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import flask
 import itertools
 from dbj import dbj
 from datetime import datetime
-import settings
+
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-settings.init()
+db = {"test": "hi"}
 
 @app.route("/")
 def home():
@@ -27,16 +27,16 @@ def get_message(currentIP, currentTime):
     requestIDRaw = currentIP + "_" + currentTime
     requestIDProcessed = currentTime.replace(":", "-").replace(".", "_")
     print(requestIDProcessed)
-    print(settings.db)
+    print(db)
 
-    if not requestIDProcessed in settings.db:
+    if not requestIDProcessed in db:
         out_dict["ipExists"] = "no"
         out_dict["timeStampExists"] = "no"
         out_dict["currentIP"] = currentIP
         out_dict["curentTimeStamp"] = currentTime
         return json.dumps(out_dict)
     else:
-        val = settings.db.get(requestIDProcessed)
+        val = db.get(requestIDProcessed)
         print(val)
         out_dict["ipExists"] = "yes"
         out_dict["timeStampExists"] = "yes"
@@ -77,12 +77,12 @@ def uploadfile():
         requestIDRaw = currentIP + "_" + currentTime
         requestIDProcessed = currentTime.replace(":", "-").replace(".", "_")
 
-        if not requestIDProcessed in settings.db:
+        if not requestIDProcessed in db:
             entry = {'stage': 0, 'page' : 0, 'total' : 0, 'output' : []}
-            settings.db[requestIDProcessed] = entry
+            db[requestIDProcessed] = entry
 
         print("Forking....")
-        thread = threading.Thread(target=main, args=(filename, requestIDProcessed))
+        thread = threading.Thread(target=main, args=(filename, db, requestIDProcessed))
         thread.start()
         return jsonify({"Succeeded": "yes", "YourIP" : str(currentIP), "YourTime" : currentTime})
 
