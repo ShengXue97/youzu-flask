13c0ead6be6348c4b4ddc0522dc78e795ee49076
ShengXue97 <simshengxue1997@gmail.com>
2020-06-08 11:00:45 +0800

    remove output.csv processing

--- MainV9.py
+++ MainV9.py
@@ -4,7 +4,6 @@ import cv2
 import numpy as np
 import pytesseract
 from PIL import Image, ImageOps
-from langdetect import detect
 from docx import Document
 from docx.shared import Inches
 from GibberishDetector import classify
