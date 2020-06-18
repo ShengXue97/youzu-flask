911aa207341e9bd4fcbf4e449c065c58b169c66f
Ubuntu <edu@edu.tw5gqksjlszu1pueartxc1ubkc.ix.internal.cloudapp.net>
2020-06-18 02:11:07 +0000

    change to v10

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, session
 from flask_cors import CORS, cross_origin
 import json
 import ast
-from MainV9 import Process
+from MainV10 import Process
 import os
 import pandas as pd
 import threading, time
@@ -11,13 +11,33 @@ import itertools
 from datetime import datetime
 import os.path
 
+# # install the following 2 libs first
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import Marshmallow
+# from models.question import Question
+
 app = Flask(__name__)
 cors = CORS(app)
+
+# db = SQLAlchemy(app)
+# ma = Marshmallow(app)
+# # change to your mysql settings here,create an empty schema before you run the code.
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your_account_name:your_password@your_mysql_address/your_database_name'
+#
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = "EFWM@!R@!@MF!!@$#^@#%#@^"
 @app.route("/")
 def home():
     return "<h1>Welcome man, enjoy your stay<h1>"
+
+# @app.route("/")
+# def home():
+#     # this step is to create a table defined in models,can put it somewhere else and add on better try except.
+#     try:
+#         db.create_all()
+#     except:
+#         print("create wrong")
+#     return "<h1>Welcome man, enjoy your stay<h1>"
     
 def get_message(currentIP, currentTime, requestIDProcessed):
     out_dict = {}
@@ -99,24 +119,45 @@ def getresult():
     requestIDProcessed = currentTime.replace(":", "-").replace(".", "_")
     
     df = pd.read_csv(requestIDProcessed + "_output.csv")
-    # Create an empty list 
-    row_json = []
-    
-    # Iterate over each row 
+    # Create required qn or pg lists
+    thisPageList = []
+    row_json = [] #[[[]]]
+    # Iterate over each row
+    currentPageNum = 1
     for index, rows in df.iterrows(): 
-        # Create list for the current row 
-        page = rows["Question"] if rows["Question"] != "" else "-"
-        ans_a = rows["A"] if rows["A"] != "" else "-"
-        ans_b = rows["B"] if rows["B"] != "" else "-"
-        ans_c = rows["C"] if rows["C"] != "" else "-"
-        ans_d = rows["D"] if rows["D"] != "" else "-"
+        # Create list for the current row
+        pageNum = rows["Page"]
+        qnNum = rows["Number"]
+        if pageNum == currentPageNum:
+            page = rows["Question"] if rows["Question"] != "" else "-"
+            ans_a = rows["A"] if rows["A"] != "" else "-"
+            ans_b = rows["B"] if rows["B"] != "" else "-"
+            ans_c = rows["C"] if rows["C"] != "" else "-"
+            ans_d = rows["D"] if rows["D"] != "" else "-"
+            qn_list =[pageNum, page, ans_a, ans_b, ans_c, ans_d, qnNum]
+            # append question list to page list
+            thisPageList.append(qn_list)
+        # once page changes, append previous page list to the final list
+        elif pageNum > currentPageNum:
+            row_json.append(thisPageList)
+            currentPageNum += 1
+            thisPageList = []
+            # append that particular first qn on the new page to the now empty PageList
+            if pageNum == currentPageNum:
+                page = rows["Question"] if rows["Question"] != "" else "-"
+                ans_a = rows["A"] if rows["A"] != "" else "-"
+                ans_b = rows["B"] if rows["B"] != "" else "-"
+                ans_c = rows["C"] if rows["C"] != "" else "-"
+                ans_d = rows["D"] if rows["D"] != "" else "-"
+                qn_list = [pageNum, page, ans_a, ans_b, ans_c, ans_d, qnNum]
+                # append question list to page list
+                thisPageList.append(qn_list)
 
-        my_list =[page, ans_a, ans_b, ans_c, ans_d] 
-        # append the list to the final list 
-        row_json.append(my_list)
+    row_json.append(thisPageList)
+
 
     os.remove(requestIDProcessed + "_output.csv")
     return jsonify(row_json)
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=3001)
