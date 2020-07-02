32e73a8a70011b0ab24001a250543f649d1b2a15
ShengXue97 <simshengxue1997@gmail.com>
2020-07-02 12:46:14 +0800

    add workspace generator

--- MainV10.py
+++ MainV10.py
@@ -926,6 +926,8 @@ class Process:
             pdf_path = "ReactPDF/" + paper_name + ".pdf"
         elif os.path.exists("/datassd/pdf_downloader-master/pdfs/" + paper_name + ".pdf"):
             pdf_path = "/datassd/pdf_downloader-master/pdfs/" + paper_name + ".pdf"
+        elif os.path.exists("/pdfs/" + paper_name + ".pdf"):
+            pdf_path = "/pdfs/" + paper_name + ".pdf"
 
         pages = convert_from_path(pdf_path)
         pg_cntr = 1
